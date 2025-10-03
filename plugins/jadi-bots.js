e63a33f7234f3e06cbe58574630cbfe1fd0902bb
xzzys26 <erenxz01@gmail.com>
2025-10-03 19:08:54 -0400

    Crear jadi-bots.js

---
+++ plugins/jadi-bots.js
@@ -0,0 +1,39 @@
+import ws from 'ws'
+
+async function handler(m, { conn: stars, usedPrefix }) {
+  let uniqueUsers = new Map()
+
+  global.conns.forEach((conn) => {
+    if (conn.user && conn.ws.socket && conn.ws.socket.readyState !== ws.CLOSED) {
+      uniqueUsers.set(conn.user.jid, conn)
+    }
+  })
+
+  let users = [...uniqueUsers.values()]
+
+  let message = users.map((v, index) => `🌷 *#${index + 1} »* ${v.user.name || 'Sin nombre~'}\n   ↳ wa.me/${v.user.jid.replace(/[^0-9]/g, '')}`).join('\n\n')
+
+  let replyMessage = message.length === 0 ? '' : message
+  let totalUsers = users.length
+  
+  let responseMessage = `🌟 *L I S T A   D E   S U B - B O T S* 🌟\n\n`
+  responseMessage += `📜 *Total de Sub-Bots activos »* ${totalUsers || '0'}\n\n`
+  
+  if (totalUsers === 0) {
+    responseMessage += `🌱 No hay Sub-Bots conectados en este momento~\n\n`
+    responseMessage += `🪷 *Tip:* Usa el comando para convertirte en Sub-Bot`
+  } else {
+    responseMessage += `${replyMessage.trim()}\n\n`
+    responseMessage += `🌸 ¡Gracias por ser parte de nuestra familia!`
+  }
+
+  await stars.sendMessage(m.chat, { 
+    text: responseMessage.trim(), 
+    ...(typeof rcanal !== 'undefined' ? rcanal : {}) 
+  }, { quoted: m })
+}
+
+handler.command = ['sockets', 'bots', 'listbots']
+handler.help = ['bots', 'sockets', 'listbots']
+handler.tags = ['jadibot']
+export default handler
