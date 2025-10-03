ae1f779bf46f6f528393f16cf31a865dc7410487
xzzys26 <erenxz01@gmail.com>
2025-10-02 22:22:10 -0400

    Actualizar info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,12 +1,12 @@
 let handler = async (m, { conn }) => {
   try {
     await conn.sendMessage(m.chat, {
-      text: '👑 *CREADOR - 𝗟𝗲𝗼  𝘅𝘇𝘅𝘀𝘆 ⚡*\n\nSelecciona una opción:',
+      text: '👑 *CREADOR - 𝗟𝗲𝗼  𝘅𝘇𝘅𝘀𝘆 ⚡*\n\𝗦𝗲𝗹𝗲𝗰𝗶𝗼𝗻𝗮 𝗨𝗻 𝗠𝗲𝘁𝗼𝗱𝗼:',
       buttons: [
-        { buttonId: '!instagram', buttonText: { displayText: '📷 Instagram' }, type: 1 },
-        { buttonId: '!tiktok', buttonText: { displayText: '🎵 TikTok' }, type: 1 },
-        { buttonId: '!grupo', buttonText: { displayText: '💬 Grupo' }, type: 1 },
-        { buttonId: '!contacto', buttonText: { displayText: '📞 Contacto' }, type: 1 }
+        { buttonId: '!instagram', buttonText: { displayText: '📸 𝐈𝐍𝐒𝐓𝐀𝐆𝐑𝐀𝐌' }, type: 1 },
+        { buttonId: '!tiktok', buttonText: { displayText: ' 💎 𝐓𝐈𝐊𝐓𝐎𝐊' }, type: 1 },
+        { buttonId: '!grupo', buttonText: { displayText: '📢 𝐂𝐀𝐍𝐀𝐋 𝐎𝐅𝐈𝐂𝐈𝐀𝐋' }, type: 1 },
+        { buttonId: '!contacto', buttonText: { displayText: '👑 𝐂𝐑𝐄𝐀𝐃𝐎𝐑' }, type: 1 }
       ]
     }, { quoted: m })
 
@@ -16,10 +16,10 @@ let handler = async (m, { conn }) => {
     const message = `
 👑 *CREADOR - 𝗟𝗲𝗼  𝘅𝘇𝘅𝘀𝘆 ⚡*
 
-📷 Instagram: instagram.com/xzzys26
-🎵 TikTok: tiktok.com/@xzzys16  
-💬 Grupo: chat.whatsapp.com/CYKX0ZR6pWMHCXgBgVoTGA
-📞 Contacto: wa.me/18493907272
+📸 𝑰𝑵𝑨𝑻𝑨𝑮𝑹𝑨𝑴: instagram.com/xzzys26
+💎 𝑻𝑰𝑲𝑻𝑶𝑲: tiktok.com/@xzzys16  
+📢 𝑪𝑨𝑵𝑨𝑳: https://whatsapp.com/channel/0029VbBBn9R4NViep4KwCT3Z
+👑 𝑪𝑹𝑬𝑨𝑫𝑶𝑹: wa.me/18493907272
     `.trim()
     await conn.sendMessage(m.chat, { text: message }, { quoted: m })
   }
@@ -33,17 +33,17 @@ handler.before = async (m) => {
   const sender = m.sender
   
   switch(selectedButton) {
-    case '📷 Instagram':
-      await m.reply('📷 *Instagram:* https://www.instagram.com/xzzys26')
+    case '📸 𝙄𝙉𝙎𝙏𝘼𝙂𝙍𝘼𝙈':
+      await m.reply('📸 *Instagram:* https://www.instagram.com/xzzys26')
       break
-    case '🎵 TikTok':
-      await m.reply('🎵 *TikTok:* https://www.tiktok.com/@xzzys16')
+    case '💎 𝙏𝙄𝙆𝙏𝙊𝙆':
+      await m.reply('💎 *TikTok:* https://www.tiktok.com/@xzzys16')
       break
-    case '💬 Grupo':
-      await m.reply('💬 *Grupo WhatsApp:* https://chat.whatsapp.com/CYKX0ZR6pWMHCXgBgVoTGA')
+    case '📢 𝘾𝘼𝙉𝘼𝙇 𝙊𝙁𝙄𝘾𝙄𝘼𝙇':
+      await m.reply('💬 *Grupo WhatsApp:* https://whatsapp.com/channel/0029VbBBn9R4NViep4KwCT3Z')
       break
-    case '📞 Contacto':
-      await m.reply('📞 *Contacto Directo:* https://wa.me/18493907272')
+    case '👑 𝘾𝙍𝙀𝘼𝘿𝙊𝙍':
+      await m.reply('📲 *Contacto Directo:* https://wa.me/18493907272')
       break
   }
 }
