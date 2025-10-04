633c587e310304329ce5ef8b68ab1237247b90be
xzzys26 <erenxz01@gmail.com>
2025-10-04 01:10:10 -0400

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -1,5 +1,5 @@
 //Creador del menu: BrayanOFC
-import fetch from 'node-fetch'
+import fetch from 'node-fetch
 
 const botname = global.botname || '🌸 𝐈𝐓𝐒𝐔𝐊𝐈 𝐍𝐀𝐊𝐀𝐍𝐎-𝐀𝐈 🌸'
 const creador = '𝗟𝗲𝗼  𝘅𝘇𝘅𝘀𝘆 ⚡'
