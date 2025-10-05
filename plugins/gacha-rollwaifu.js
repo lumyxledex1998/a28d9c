4e2f601e99164dcf74eed705d0eb47eaefaf7a61
𝐋𝐞𝐨    𝐗𝐳𝐳𝐲  ᵒᶠⁱᶜⁱᵃˡ ✨ <erenxz01@gmail.com>
2025-10-05 18:33:27 -0400

    Actualizar gacha-rollwaifu.js

--- plugins/gacha-rollwaifu.js
+++ plugins/gacha-rollwaifu.js
@@ -10,7 +10,7 @@ async function loadCharacters() {
         const data = await fs.readFile(charactersFilePath, 'utf-8')
         return JSON.parse(data)
     } catch (error) {
-        throw new Error('❀ No se pudo cargar el archivo characters.json.')
+        throw new Error('🧧 No se pudo cargar el archivo characters.json.')
     }
 }
 
@@ -18,7 +18,7 @@ async function saveCharacters(characters) {
     try {
         await fs.writeFile(charactersFilePath, JSON.stringify(characters, null, 2), 'utf-8')
     } catch (error) {
-        throw new Error('❀ No se pudo guardar el archivo characters.json.')
+        throw new Error('🧧 No se pudo guardar el archivo characters.json.')
     }
 }
 
@@ -35,7 +35,7 @@ async function saveHarem(harem) {
     try {
         await fs.writeFile(haremFilePath, JSON.stringify(harem, null, 2), 'utf-8')
     } catch (error) {
-        throw new Error('❀ No se pudo guardar el archivo harem.json.')
+        throw new Error('🧧 No se pudo guardar el archivo harem.json.')
     }
 }
 
@@ -47,7 +47,7 @@ let handler = async (m, { conn }) => {
         const remainingTime = Math.ceil((cooldowns[userId] - now) / 1000)
         const minutes = Math.floor(remainingTime / 60)
         const seconds = remainingTime % 60
-        return await conn.reply(m.chat, `《✧》Debes esperar *${minutes} minutos y ${seconds} segundos* para usar *#rw* de nuevo.`, m)
+        return await conn.reply(m.chat, `《🌟》Debes esperar *${minutes} minutos y ${seconds} segundos* para usar *#rw* de nuevo.`, m)
     }
 
     try {
