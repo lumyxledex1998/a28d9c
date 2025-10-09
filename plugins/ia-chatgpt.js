723691e74c6ea9473836f1c5eeee2c81f70520f4
𝐋𝐞𝐨    𝐗𝐳𝐳𝐲  ᵒᶠⁱᶜⁱᵃˡ ✨ <erenxz01@gmail.com>
2025-10-08 21:37:28 -0400

    Actualizar ia-chatgpt.js

--- plugins/ia-chatgpt.js
+++ plugins/ia-chatgpt.js
@@ -5,20 +5,6 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     return conn.reply(m.chat, `🌸 ¡Hola! ¿cómo puedo ayudarte hoy?`, m, rcanal);
   }
 
-    const res = await fetch('https://files.catbox.moe/j65sl7.jpg');
-    const thumb2 = Buffer.from(await res.arrayBuffer());
-
-    const fkontak = {
-      key: { participants: "0@s.whatsapp.net", remoteJid: "status@broadcast", fromMe: false, id: "Halo" },
-      message: {
-        locationMessage: {
-          name: `𝗖𝗛𝗔𝗧𝗚𝗣𝗧 𝗙𝗥𝗢𝗠 𝗢𝗣𝗘𝗡𝗔𝗜`,
-          jpegThumbnail: thumb2
-        }
-      },
-      participant: "0@s.whatsapp.net"
-    };
-
   try {
     const url = `https://api.kirito.my/api/chatgpt?q=${encodeURIComponent(text)}&apikey=by_deylin`;
     const res = await fetch(url);
@@ -28,7 +14,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
       return conn.reply(m.chat, "❌ No recibí respuesta de la IA, intenta de nuevo.", m, fake);
     }
 
-    await conn.reply(m.chat, `${data.response}`, fkontak, rcanal);
+    await conn.reply(m.chat, `${data.response}`, m, rcanal);
   } catch (e) {
     console.error(e);
     await conn.reply(m.chat, "⚠️ Hubo un error al conectar con la IA.", m, fake);
