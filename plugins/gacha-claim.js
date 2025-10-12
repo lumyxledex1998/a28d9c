9add73edbea0ad6e9d1b3977294415c4fcfca7be
𝐋𝐞𝐨    𝐗𝐳𝐳𝐲  ᵒᶠⁱᶜⁱᵃˡ ✨ <erenxz01@gmail.com>
2025-10-11 22:11:13 -0400

    Actualizar gacha-claim.js

--- plugins/gacha-claim.js
+++ plugins/gacha-claim.js
@@ -53,7 +53,7 @@ let handler = async (m, { conn }) => {
     if (m.quoted && m.quoted.sender === conn.user.jid) {
         try {
             const characters = await loadCharacters();
-            const characterIdMatch = m.quoted.text.match(/🆔️ ID: \*(.+?)\*/);
+            const characterIdMatch = m.quoted.text.match(/🪪 ID: \*(.+?)\*/);
 
             if (!characterIdMatch) {
                 await conn.reply(m.chat, '《🌟》No se pudo encontrar el ID del personaje en el mensaje citado.', m);
