ed1e30cd53878f8477889491a7ba17e389aca7bf
𝐒𝐨𝐲𝕄𝕒𝕪𝕔𝕠𝕝 ᵒᶠⁱᶜⁱᵃˡ <karatekidamericatv@gmail.com>
2025-10-12 20:41:02 -0500

    Update index.js

--- index.js
+++ index.js
@@ -25,9 +25,9 @@ global.prefixes = Array.isArray(config.prefix) ? [...config.prefix] : []
 global.owner = Array.isArray(config.owner) ? config.owner : []
 global.opts = global.opts && typeof global.opts === 'object' ? global.opts : {}
 
-if (!existsSync("./tmp")) {
-  mkdirSync("./tmp");
-}
+if (!fs.existsSync("./tmp")) {
+  fs.mkdirSync("./tmp");
+  }
 
 const CONFIG_PATH = path.join(__dirname, 'config.js')
 watchFile(CONFIG_PATH, async () => {
