72bc098d0ad3d1a065bfd2026facf89f7627e58f
Legna ❀ <Beatrizmanjarreztorres6@gmail.com>
2025-10-21 16:38:32 -0700

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -62,9 +62,7 @@ let handler = async (m, { conn, usedPrefix: _p }) => {
 
     // Detectar automáticamente si es bot oficial o sub-bot
     const botJid = conn.user.jid
-        const officialBotNumber = [ '18292605400@s.whatsapp.net',
-'521667510398@s.whatsapp.net'
-];
+        const officialBotNumber = '18292605400@s.whatsapp.net'
 
 let name = conn.getName(m.sender) || 'Usuario'
 let taguser = '@' + m.sender.split('@')[0]
