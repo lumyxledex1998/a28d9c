55642428a20c356c20a326a01349327def747c88
Legna ❀ <Beatrizmanjarreztorres6@gmail.com>
2025-10-22 15:15:54 -0700

    Crear tools-setmeta.js

---
+++ plugins/tools-setmeta.js
@@ -0,0 +1,35 @@
+let handler = async (m, { text, usedPrefix, command }) => {
+const userId = m.sender
+if (command === 'setmeta') {
+const packParts = text.split(/[\u2022|]/).map(part => part.trim())
+if (packParts.length < 2) {
+return m.reply(`🌟 *Envia un texto para que itsuki asigne el pack y el autor*.\n> Ejemplo: *${usedPrefix + command} Istuki • Leo*`)
+}
+const packText1 = packParts[0]
+const packText2 = packParts[1]
+if (!global.db.data.users[userId]) {
+global.db.data.users[userId] = {}
+}
+const packstickers = global.db.data.users[userId]
+packstickers.text1 = packText1
+packstickers.text2 = packText2
+await global.db.write()
+return m.reply(`🍓 *Se actualizo el pack y autor por defecto para tus stickers*.`);
+}
+if (command === 'delmeta') {
+if (!global.db.data.users[userId] || (!global.db.data.users[userId].text1 && !global.db.data.users[userId].text2)) {
+return m.reply(`🌟 *No tienes establecido un pack de stickers*.`)
+}
+const packstickers = global.db.data.users[userId]
+delete packstickers.text1
+delete packstickers.text2
+await global.db.write()
+return m.reply(`🍓 *Se restablecio el pack y autor por defecto para tus stickers*.`)
+}}
+
+handler.help = ['setmeta', 'delmeta']
+handler.tags = ['tools']
+handler.command = ['setmeta', 'delmeta']
+handler.group = true
+
+export default handler
