e349d8be51e4bb968b5ef5830824023390fbbafd
Legna ❀ <Beatrizmanjarreztorres6@gmail.com>
2025-10-22 15:33:18 -0700

    Actualizar tools-setmeta.js

--- plugins/tools-setmeta.js
+++ plugins/tools-setmeta.js
@@ -8,7 +8,7 @@ const ctxOk = (global.rcanalr || {})
 if (command === 'setmeta') {
 const packParts = text.split(/[\u2022|]/).map(part => part.trim())
 if (packParts.length < 2) {
-return m.reply('🌟 *Envia un texto para que itsuki asigne el pack y el autor*.\n> Ejemplo: *${usedPrefix + command} Istuki • Leo*', m, rcanal) 
+return m.reply('🌟 *Envia un texto para que itsuki asigne el pack y el autor*.\n> Ejemplo: *${usedPrefix + command} Istuki • Leo*', m, ctxErr) 
 }
 const packText1 = packParts[0]
 const packText2 = packParts[1]
@@ -19,17 +19,17 @@ const packstickers = global.db.data.users[userId]
 packstickers.text1 = packText1
 packstickers.text2 = packText2
 await global.db.write()
-return m.reply('🍓 *Se actualizo el pack y autor por defecto para tus stickers*', m, rcanal) 
+return m.reply('🍓 *Se actualizo el pack y autor por defecto para tus stickers*', m, ctxErr) 
 }
 if (command === 'delmeta') {
 if (!global.db.data.users[userId] || (!global.db.data.users[userId].text1 && !global.db.data.users[userId].text2)) {
-return m.reply('🌟 *No tienes establecido un pack de stickers', m, rcanal)
+return m.reply('🌟 *No tienes establecido un pack de stickers', m, ctxErr)
 }
 const packstickers = global.db.data.users[userId]
 delete packstickers.text1
 delete packstickers.text2
 await global.db.write()
-return m.reply('🍓 *Se restablecio el pack y autor por defecto para tus stickers', m, rcanal)
+return m.reply('🍓 *Se restablecio el pack y autor por defecto para tus stickers', m, ctxErr)
 }}
 
 handler.help = ['setmeta', 'delmeta']
